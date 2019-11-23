5ba3140eb488f9c6fe1c38ccb940cd505dd781d0
cprybell <cprybell@sbcglobal.net>
2019-11-23 15:35:10 -0800

    Added most functionality. Working on push command at the moment

--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -5,4 +5,29 @@ module.exports = function(app) {
     app.get("/api/friends", function(req,res) {
         res.json(friendsData);
     });
+
+    app.post("/api/friends", function(req, res) {
+        if (isValidInput(req.body)) {
+            friendsData.push(req.body);
+            res.json(true);
+        }
+        else {
+            res.json(false);
+        }
+    });
+}
+
+function isValidInput(friendObject) {
+    if(friendObject.name === "") {
+        return false;
+    }
+    if (friendObject.photo === "") {
+        return false;
+    }
+    for (i = 0; i < friendObject.scores.length; i++) {
+        if (friendObject.scores[i] === NaN) {
+            return false;
+        }
+    }
+    return true;
 }
