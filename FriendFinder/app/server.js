8679b269a88ab81a16f1030041b3dad5db17a6a5
cprybell <cprybell@sbcglobal.net>
2019-11-23 10:06:20 -0800

    Home.html file is set-up mostly. Need to add link to server route

--- FriendFinder/app/server.js
+++ FriendFinder/app/server.js
@@ -2,15 +2,21 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-var port = 8080;
+var PORT = 8080;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
 app.get("/", function(req, res) {
+    console.log("home");
     res.sendFile(path.join(__dirname, "public", "home.html"));
 });
 
 app.get("/survey", function(req,res) {
+    console.log("survey");
     res.sendFile(path.join(__dirname, "public", "survey.html"));
+});
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
 });
